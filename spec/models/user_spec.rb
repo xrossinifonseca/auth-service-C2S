64ecb017bf0b55f773bf6343a35f8c2e7eb1cb60
Rossini fonseca <rossinidrum@outlook.com>
2024-10-31 11:41:03 -0300

    test: add model tests for User validations
    - Test presence validations for name, email, and password
    - Verify minimum password length of 6 characters
    - Check for uniqueness and format of email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+
+  context "validations" do
+
+    describe "valid user" do
+
+      it "is valid with a unique email" do
+        user = build(:user)
+        expect(user).to be_valid
+      end
+    end
+
+    describe "validations for email presence and uniqueness" do
+
+      it "is not valid if the email is blank" do
+        user = User.new(email: nil)
+        expect(user).not_to be_valid
+        expect(user.errors[:email]).to include("can't be blank")
+      end
+
+      it "is not valid if the email is not unique" do
+
+        user_1 = User.create(name:"john",email: "john.doe@example.com",password:"password")
+        user = User.create(name:"john",email: "john.doe@example.com",password:"password")
+        expect(user).not_to be_valid
+        expect(user.errors[:email]).to include("has already been taken")
+      end
+    end
+
+  describe "Validations for password and name" do
+
+    it "Is not valid if the name is blank" do
+      user = User.new(name: nil)
+      expect(user).not_to be_valid
+      expect(user.errors[:name]).to include("can't be blank")
+    end
+
+    it "Is not valid if the password is blank" do
+      user = User.new(password: nil)
+      expect(user).not_to be_valid
+      expect(user.errors[:password]).to include("can't be blank")
+    end
+
+    it "is not valid if the password has less than 6 characters" do
+      user = build(:user, password: "12345")
+      expect(user).not_to be_valid
+      p user.errors[:password]
+      expect(user.errors[:password]).to include("is too short (minimum is 6 characters)")
+    end
+  end
+  end
 end
