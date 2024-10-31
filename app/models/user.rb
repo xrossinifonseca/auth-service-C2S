965e90a5b280b4d1413378aa71be23050fbc9570
Rossini fonseca <rossinidrum@outlook.com>
2024-10-31 11:40:08 -0300

    feat: add User model with validations and schema - Include validations for:   - Presence of name, email, and password   - Minimum password length of 6 characters   - Unique and correctly formatted email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+
+  has_secure_password
+
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :name, presence: true
+
+
+
 end
